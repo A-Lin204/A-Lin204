439b00621eecaf5577a45e7b808b188b062e8322
A Lin <myot21087@gmail.com>
2024-08-19 22:17:19 +0700

    Hello

---
+++ AnimalInheritance.java
@@ -0,0 +1,9 @@
+package animal1;
+
+public class AnimalInheritance {
+ private boolean vegetarian;
+ private String eats;
+ private int noOfLegs;
+ 	
+ 	
+}
---
+++ Cat.java
@@ -0,0 +1,11 @@
+package animal1;
+
+public class Cat extends AnimalInheritance{
+	private String color;
+	public Cat(String color,boolean veg, String food, int legs) {
+	//	this.super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
